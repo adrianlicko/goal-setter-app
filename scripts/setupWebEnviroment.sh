e8ad4cf5d541d98df22c9f3c19a3c27cc6613a15
AdrianLicko <licko.adrian03@gmail.com>
2024-05-12 18:06:46 +0200

    Added new script setupWebEnviroment.sh

---
+++ scripts/setupWebEnviroment.sh
@@ -0,0 +1,15 @@
+cd ..
+cd 'Goal Setter app'
+
+# Instalacia "zavislosti" pre backend
+npm install
+
+# Instalacia "zavislosti" pre frontend
+cd frontend
+npm install
+
+# Stavba frontendu
+npm run build
+
+# Spustenie celej aplikacie
+npm start
